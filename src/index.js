c61a6c4e8e97438d6800115364a38614e26ba020
Pat Cavit <pcavit@gmail.com>
2014-11-25 15:24:23 -0800

    Back to commander, also using right aws lib
    docopt appears to be broken for varidic arguments in node.
    
    Also, :coffee: script barf.

--- src/index.js
+++ src/index.js
@@ -3,47 +3,72 @@
 
 // TODO: support creds from ENV as AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
 
-var fs   = require("fs"),
-    path = require("path"),
-    args;
+var program = require("commander"),
+    access  = {
+        key    : process.env.AWS_ACCESS_KEY_ID,
+        secret : process.env.AWS_SECRET_ACCESS_KEY
+    };
 
-/*
-Sync53, a more sane way to manage Route53
+function options() {
+    var opts = {};
 
-Usage:
-    sync53 import [options]
-    sync53 import [options] [<zones> ...]
-    sync53 export <file>
-    sync53 export [options] <file>
-    sync53 export [options] <file> [<zones>...]
-    sync53 -h | --help | --version
-    sync53 --version
+    program.options.forEach(function(option) {
+        var name = option.long.replace(/^--/, "");
 
-Options:
-    # General Options
-    -h, --help     Show this screen.
-    --version      Show version.
-    -v, --verbose  Verbose output
-    --silent       Only errors will be output
-    
-    # AWS Details
-    -k <key>, --key <key>           AWS Access Key
-    -s <secret>, --secret <secret>  AWS Secret Access Key
+        if(name === "version") {
+            return;
+        }
 
-    # Import Options
-    -o <file>, --output <file>      Save imported config to a file
+        if(name in program) {
+            opts[name] = program[name];
+        }
+    });
 
-Arguments:
-    
-*/
-
-
-// Welp, this call looks screwy. fdocopt returns a function from its module.exports
-// which is itself a function that you have to invoke. *sigh* I just don't know...
-args = require("fdocopt")()(__filename, { version : require("../package.json").version });
-
-if(args.import) {
-    require("./actions/import")(args);
-} else {
-    require("./actions/export")(args);
+    return opts;
 }
+
+function merge() {
+    var result = {},
+        size   = arguments.length,
+        i, key, obj;
+
+    for(i = 0; i < size; i++) {
+        obj = arguments[i];
+
+        for(key in obj) {
+            result[key] = obj[key];
+        }
+    }
+
+    return result;
+}
+
+program
+    .version(require("../package.json").version)
+    .option("-v, --verbose", "Verbose output")
+    .option("--silent", "Silent output (only errors)")
+    .option("--proxy <proxy>", "URL to proxy requests through")
+    .option("-k, --key <key>", "AWS Access Key")
+    .option("-s, --secret <secret>", "AWS Secret");
+
+program
+    .command("import [zones...]")
+    .description("Import DNS information from Route53")
+    .option("-o, --output <file>", "Save imported config to a file")
+    .action(function(zones) {
+        require("./actions/import")(merge(access, options(), {
+            zones : zones
+        }));
+    });
+
+program
+    .command("export <file> [zones...]")
+    .description("Write the config stored in file to Route53")
+    .action(function(file, zones) {
+        require("./actions/export")(merge(access, options(), {
+            file  : file,
+            zones : zones
+        }));
+    });
+
+program.parse(process.argv);
