ec85bbb6a3199aaa02deade4a4d999c25b57c338
Pat Cavit <pcavit@gmail.com>
2014-11-25 13:45:13 -0800

    fdocopt is pretty swell

--- src/index.js
+++ src/index.js
@@ -5,12 +5,42 @@
 
 var fs   = require("fs"),
     path = require("path"),
-    args = require("docopt").docopt(
-        // Use path.resolve here because otherwise it reads from the root which
-        // feels wrong. Especially given that require works differently.
-        fs.readFileSync(path.resolve(__dirname, "../cli.txt"), "utf8"),
-        { version : require("../package.json").version }
-    );
+    args;
+
+/*
+Sync53, a more sane way to manage Route53
+
+Usage:
+    sync53 import [options]
+    sync53 import [options] [<zones> ...]
+    sync53 export <file>
+    sync53 export [options] <file>
+    sync53 export [options] <file> [<zones>...]
+    sync53 -h | --help | --version
+    sync53 --version
+
+Options:
+    # General Options
+    -h, --help     Show this screen.
+    --version      Show version.
+    -v, --verbose  Verbose output
+    --silent       Only errors will be output
+    
+    # AWS Details
+    -k <key>, --key <key>           AWS Access Key
+    -s <secret>, --secret <secret>  AWS Secret Access Key
+
+    # Import Options
+    -o <file>, --output <file>      Save imported config to a file
+
+Arguments:
+    
+*/
+
+
+// Welp, this call looks screwy. fdocopt returns a function from its module.exports
+// which is itself a function that you have to invoke. *sigh* I just don't know...
+args = require("fdocopt")()(__filename, { version : require("../package.json").version });
 
 if(args.import) {
     require("./actions/import")(args);
