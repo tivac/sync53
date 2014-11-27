0e7e5e963a73dd127e7006fc4d71014c9b61b792
Pat Cavit <pcavit@gmail.com>
2014-11-26 16:42:14 -0800

    Stricter validation on AWS configs :new_moon_with_face:

--- src/validators/aws/record.js
+++ src/validators/aws/record.js
@@ -8,22 +8,44 @@ module.exports = joi.object({
     Name : lib.str,
     Type : lib.type,
     TTL  : joi.number().integer(),
-    Weight: joi.number().integer(),
-    AliasTarget : {
-        DNSName: lib.str.required(),
-        EvaluateTargetHealth: joi.boolean().required(),
-        HostedZoneId: lib.str.required()
-    },
-    Failover: joi.string().valid([ "PRIMARY", "SECONDARY" ]),
-    GeoLocation: {
-        ContinentCode: lib.continent,
-        CountryCode: lib.str,
-        SubdivisionCode: lib.str
-    },
-    HealthCheckId: lib.str,
-    Region: lib.region,
-    ResourceRecords: joi.array().includes({
+    HealthCheckId : lib.str,
+    ResourceRecords : joi.array().includes({
         Value : lib.str.required()
     }),
-    SetIdentifier: lib.str
+    
+    // Alias
+    AliasTarget : joi.object().keys({
+        DNSName : lib.str.required(),
+        EvaluateTargetHealth : joi.boolean().required(),
+        HostedZoneId : lib.str.required()
+    }),
+
+    // Required for any routing strategies
+    SetIdentifier : lib.str
+        .when("Region",      { is : joi.exist(), then : lib.str.required() })
+        .when("GeoLocation", { is : joi.exist(), then : lib.str.required() })
+        .when("Weight",      { is : joi.exist(), then : lib.str.required() })
+        .when("Failover",    { is : joi.exist(), then : lib.str.required() }),
+
+    // Weight-based routing
+    Weight : joi.number().integer(),
+    
+    // Failover-based routing
+    Failover : joi.string().valid([ "PRIMARY", "SECONDARY" ]),
+    
+    // Geolocation routing
+    GeoLocation : joi.object().keys({
+        ContinentCode : lib.continent,
+        
+        // Can't specify a country when continent is defined
+        CountryCode : lib.str
+            .when("ContinentCode", { is : joi.exist(), then : joi.forbidden() }),
+        
+        // Can't specify an area when country isn't defined
+        SubdivisionCode : lib.str
+            .when("CountryCode",   { is : joi.exist(), otherwise : joi.forbidden() })
+    }),
+
+    // Latency-based routing
+    Region : lib.region
 });
