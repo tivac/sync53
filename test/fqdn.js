0676de57e99921232170e49bddea6ec50b4a2097
Pat Cavit <pcavit@gmail.com>
2014-12-01 00:03:11 -0800

    Start of some tests :construction_worker:

---
+++ test/fqdn.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var assert = require("assert"),
+    fqdn   = require("../src/fqdn.js");
+    
+describe("FQDN", function() {
+    describe(".add()", function() {
+        it("should add a trailing period", function() {
+            var out = fqdn.add("tivac.com");
+            
+            assert.equal(out, "tivac.com.");
+        });
+    });
+    
+    describe(".remove()", function() {
+        it("should remove a trailing period", function() {
+            assert.equal(fqdn.remove("tivac.com."), "tivac.com");
+        });
+    });
+});
