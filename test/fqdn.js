f05ac6faf0b5e0db3a97b9f24be967f127efe402
Pat Cavit <pcavit@gmail.com>
2015-06-25 16:23:30 -0700

    Restructure FQDN tests

--- test/fqdn.js
+++ test/fqdn.js
@@ -3,18 +3,20 @@
 var assert = require("assert"),
     fqdn   = require("../src/fqdn.js");
     
-describe("FQDN", function() {
-    describe(".add()", function() {
-        it("should add a trailing period", function() {
-            var out = fqdn.add("tivac.com");
-            
-            assert.equal(out, "tivac.com.");
+describe("lib", function() {
+    describe("FQDN", function() {
+        describe(".add()", function() {
+            it("should add a trailing period", function() {
+                var out = fqdn.add("tivac.com");
+                
+                assert.equal(out, "tivac.com.");
+            });
         });
-    });
-    
-    describe(".remove()", function() {
-        it("should remove a trailing period", function() {
-            assert.equal(fqdn.remove("tivac.com."), "tivac.com");
+        
+        describe(".remove()", function() {
+            it("should remove a trailing period", function() {
+                assert.equal(fqdn.remove("tivac.com."), "tivac.com");
+            });
         });
     });
 });
