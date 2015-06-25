5643922444fb65cc22513e3e3837725b82428c6c
Pat Cavit <pcavit@gmail.com>
2015-06-25 16:23:40 -0700

    Stub out tests for merge lib

---
+++ test/merge.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var assert    = require("assert"),
+    transform = require("../src/merge");
+    
+describe("lib", function() {
+    describe("merge", function() {
+            
+    });
+});
